3db772e9eb028d31caf8466647eb1017e6c0ae9a
octavian22 <stas_petrov_01@mail.ru>
2021-09-25 11:18:02 +0500

    ss

--- public/js/main.js
+++ public/js/main.js
@@ -6,13 +6,13 @@ $(function() {
 
   	var $this = $(this);
 
-  	
+
 
   	if ( $('body').hasClass('show-sidebar') ) {
   		$('body').removeClass('show-sidebar');
   		$this.removeClass('active');
   	} else {
-  		$('body').addClass('show-sidebar');	
+  		$('body').addClass('show-sidebar');
   		$this.addClass('active');
   	}
 
@@ -29,34 +29,45 @@ $(function() {
 				$('body').find('.js-menu-toggle').removeClass('active');
 			}
     }
-	}); 
+	});
 
-    
+
 
 });
 
 
-var popCanvas = document.getElementById("popChart");
-var barChart = new Chart(popCanvas, {
-  type: 'bar',
-  data: {
-    labels: ["China", "India", "United States", "Indonesia", "Brazil", "Pakistan", "Nigeria", "Bangladesh", "Russia", "Japan"],
-    datasets: [{
-      label: 'Уровень бедности',
-      data: [1379302771, 1281935911, 326625791, 260580739, 207353391, 204924861, 190632261, 157826578, 142257519, 126451398],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.6)',
-        'rgba(54, 162, 235, 0.6)',
-        'rgba(255, 206, 86, 0.6)',
-        'rgba(75, 192, 192, 0.6)',
-        'rgba(153, 102, 255, 0.6)',
-        'rgba(255, 159, 64, 0.6)',
-        'rgba(255, 99, 132, 0.6)',
-        'rgba(54, 162, 235, 0.6)',
-        'rgba(255, 206, 86, 0.6)',
-        'rgba(75, 192, 192, 0.6)',
-        'rgba(153, 102, 255, 0.6)'
-      ]
-    }]
-  }
-});
+const populations = () => {
+    return fetch(`${window.location.origin}/api/get_population`).then(data => {
+        return data.json()
+    })
+}
+populations().then(data => {
+    const population = data.populations
+    for (let key of population) {
+        console.log(key)
+    }
+    var popCanvas = document.getElementById("popChart");
+    var barChart = new Chart(popCanvas, {
+        type: 'bar',
+        data: {
+            labels: [data.populations],
+            datasets: [{
+                label: 'Уровень бедности',
+                data: [1379302771, 1281935911, 326625791, 260580739, 207353391, 204924861, 190632261, 157826578, 142257519, 126451398],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)',
+                    'rgba(255, 159, 64, 0.6)',
+                    'rgba(255, 99, 132, 0.6)',
+                    'rgba(54, 162, 235, 0.6)',
+                    'rgba(255, 206, 86, 0.6)',
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(153, 102, 255, 0.6)'
+                ]
+            }]
+        }
+    });
+})
--- resources/views/pages/total_amount_of_poverty.blade.php
+++ resources/views/pages/total_amount_of_poverty.blade.php
@@ -1,17 +1,12 @@
 @extends('app')
 @section('title', 'Главная')
 @section('content')
-    <div style="display: none">
-        @foreach($populations as $population)
-            <div>{{ $population->title }}</div>
-        @endforeach
-    </div>
     <div class="container">
         <div class="d-flex align-items-center justify-content-center pt-5">
             <h1 class="shadow-lg pl-5 pr-5 pt-2 pb-2">Число бедности</h1>
         </div>
         <div class="chart-block">
-            <canvas id="popChart" width="600" height="200"></canvas>
+            <canvas id="popChart" width="600" height="200">
         </div>
     </div>
 @endsection
